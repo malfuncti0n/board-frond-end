cb9d2955a732a8a3272c0f6ff3b50a283bc1f289
malfuncti0n <savvaniss@yahoo.gr>
2017-03-11 17:39:38 +0200

    add and register auth controller

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,6 +3,7 @@
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
+import { AuthController } from './auth/auth.controller';
 import { MainController } from './main/main.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
@@ -18,5 +19,6 @@ angular.module('board', ['ui.router', 'ui.bootstrap', 'toastr'])
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
+  .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
