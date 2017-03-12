4f9076468ac335549a60a4f91824f5880ddf3bb8
malfuncti0n <savvaniss@yahoo.gr>
2017-03-12 10:28:18 +0200

    fix minor bug

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -1,24 +1,22 @@
 export class AuthController {
+
     constructor($auth) {
         'ngInject';
+
         this.$auth = $auth;
     }
-    //register function satelizer
+
     register() {
-        //vm is view model
         var vm = this;
         this.$auth.signup(this.user).then(function (token) {
             vm.$auth.setToken(token);
         });
     }
-    //login function satelizer
 
-        login() {
-        //vm is view model
+    login() {
         var vm = this;
         this.$auth.login(this.login.user).then(function (token) {
             vm.$auth.setToken(token);
         });
     }
-
 }
